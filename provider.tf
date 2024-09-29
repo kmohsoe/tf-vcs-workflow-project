b1031edf57d7745130bebeddf820ff6c4674c7be
Moh Moh <kmohsoe@gmail.com>
2024-09-29 12:12:59 +0900

    add terraform config

---
+++ provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.1.0"
+    }
+      required_version = ">= 1.0.0"
+    
+    variable "aws_region"{
+
+}    }
+  }
+
+}
+
---
+++ variable.tf
@@ -0,0 +1,10 @@
+variable "aws_access_key" {
+  type        = string
+  description = "AWS access key used to create infrastructure"
+}
+
+# Required
+variable "aws_secret_key" {
+  type        = string
+  description = "AWS secret key used to create AWS infrastructure"
+}
